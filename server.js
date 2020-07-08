79186285d8ff8444f23e9cae4fa82db5541ae9b1
Anuragh19 <anuraghyarlagadda@gmail.com>
2020-07-08 22:10:35 +0530

    Removed bug

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
 
-  app.get("*", (res, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
